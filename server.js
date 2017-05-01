1783d4e04ce4a53c4c80a4a57010441e1afdb27d
Alex Troupe <adtroupe@uw.edu>
2017-04-30 17:00:27 -0700

    making progress

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var testString = "";
+
 var user = JSON.parse('{ "0" : ["peanut", "dairy"],' +
   '"1" : ["soy"],' +
   '"2" : ["egg"]}');
@@ -22,31 +24,34 @@ function getDrtiInfo(callback) {
 	});
 };
 
-// function compareRestrictions(str, callback) {
-// 	var ingredients = JSON.parse(str).nf_ingredient_statement;
-// 	var ingArray = ingredients.split(', ');	
-// 	var userAndRestriction = '';
-// 	getDrtiInfo(function(object) {
-// 		for(var subuser in user) {
-// 			user[subuser].forEach(function(dr) {
-// 				var drIngredients = object.child(dr.toLowerCase()).val();
-// 				ingArray.forEach(function(i) {
-// 					drIngredients.forEach(function(i2) {
-// 						var regex = new RegExp("\b"+i2+"\b", "ig");
-// 						if (regex.test(i)) {
-// 							if (userAndRestriction != '') {
-// 								userAndRestriction.concat(",", i, ":", dr);
-// 							} else {
-// 								userAndRestriction.concat(i, ":", dr);
-// 							};
-// 						};
-// 					});
-// 				});
-// 			});
-// 		};
-// 		callback(userAndRestriction);
-// 	});
-// }
+function compareRestrictions(str, callback) {
+	var ingredients = JSON.parse(str).nf_ingredient_statement;
+	var ingArray = ingredients.split(', ');	
+	var userAndRestriction = '';
+	getDrtiInfo(function(object) {
+		for(var x = 1; x <= Object.keys(user).length; x++) {
+			testString += x.toString();
+			user[x.toString()].forEach(function(dr) {
+				testString += dr;
+				var drIngredients = object.child(dr.toLowerCase()).val();
+				ingArray.forEach(function(i) {
+					drIngredients.forEach(function(i2) {
+						testString += i2;
+						var regex = new RegExp("\b"+i2+"\b", "ig");
+						if (regex.test(i)) {
+							if (userAndRestriction != '') {
+								userAndRestriction.concat(",", i, ":", dr);
+							} else {
+								userAndRestriction.concat(i, ":", dr);
+							};
+						};
+					});
+				});
+			});
+		};
+		callback(userAndRestriction);
+	});
+}
 
 app.get('/upc/:upcCode', function(req, res) {
 	//details of api call with upc code
@@ -79,7 +84,7 @@ app.get('/upc/:upcCode', function(req, res) {
 			// });
 
 			//Gets only api returned string
-			res.send(user["0"] + " : " + Object.keys(user).length);
+			res.send(testString);
 
 			//Breaks up api ingredients
 			// var ingredients = JSON.parse(str).nf_ingredient_statement;
